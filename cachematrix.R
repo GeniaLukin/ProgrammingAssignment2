a412da0e8c88c5f52694f24798b75e285125ef79
GeniaLukin <112501932+GeniaLukin@users.noreply.github.com>
2023-02-23 00:32:19 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -13,11 +13,12 @@ makeCacheMatrix <- function(x = matrix()) {
     x <<- y
     inv <<- NULL
   }
-  get <- function() x
+  
+  get <- function()x
   setinv <- function(inverse) inv <<- inverse
-  getinv <- function() inv
-  list(set = set, get = get, 
-  setinv = setinv, 
+  getinv <- function()inv
+  list(set = set, get = get,
+  setinv = setinv,
   getinv = getinv)
 }
 
