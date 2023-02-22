37dd48f7a022a3605b1c2827ff53b5f939c7e28b
GeniaLukin <112501932+GeniaLukin@users.noreply.github.com>
2023-02-23 00:27:22 +0200

    Add files via upload
    With complete assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+#RProgramming Assignment 2 - Lexical Scoping
 
-## Write a short comment describing this function
+## The functions create a matrix, and then set its inverse 
+## and get the inverse of be used in the next function. The
+## second function then gets the matrix generated in the function
+## from the cache and inverts it and displays it.
+
+## This function creates a matrix for future caching
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get, 
+  setinv = setinv, 
+  getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## This function calculates the inverse of the matrix 
+## produced by the frst function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached result")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
